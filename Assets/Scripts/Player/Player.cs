c55b0c88347a5b97b15b44ba875c63b7dbcb8a25
TheAkseon <abqwerty390@gmail.com>
2023-05-01 16:04:37 +0300

    add state machine

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -12,6 +12,8 @@ public class Player : MonoBehaviour
     private int _currentHealth;
     private Animator _animator;
 
+    public int Money { get; private set; }
+
     private void Start()
     {
         _currentWeapon = _weapons[0];
@@ -25,5 +27,20 @@ public class Player : MonoBehaviour
         {
             _currentWeapon.Shoot(_shootPoint);
         }
+    }
+
+    public void ApplyDamage(int damage)
+    {
+        _currentHealth -= damage;
+
+        if(_currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnEnemyDied(int reward)
+    {
+        Money += reward;
     }
 }
